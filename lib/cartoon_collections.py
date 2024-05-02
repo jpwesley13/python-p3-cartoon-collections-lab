52f85f13391d67701f822fa392408153ec5634d8
Jared Patterson <jpwesley34@gmail.com>
2024-05-02 12:49:48 -0700

    Passes tests.

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,18 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for dwarf in dwarves:
+        print(f"{dwarves.index(dwarf) + 1}. {dwarf}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return [f"{call.capitalize()}!" for call in planeteer_calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(strings):
+    for ingredient in strings:
+        if ingredient == "gouda" or ingredient == "cheddar" or ingredient == "camembert":
+            return ingredient
+    return None
